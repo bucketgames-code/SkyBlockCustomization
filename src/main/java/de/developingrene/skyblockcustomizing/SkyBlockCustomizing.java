69fa3d7ee9b4491019eef7e2320b3be76eca998f
bucketgames-code <litebucket@gmail.com>
2022-03-04 16:37:53 +0100

    Webinterface config was added

--- src/main/java/de/developingrene/skyblockcustomizing/SkyBlockCustomizing.java
+++ src/main/java/de/developingrene/skyblockcustomizing/SkyBlockCustomizing.java
@@ -3,11 +3,14 @@ package de.developingrene.skyblockcustomizing;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SkyBlockCustomizing extends JavaPlugin {
+    private static SkyBlockCustomizing plugin;
 
     @Override
     public void onEnable() {
+        plugin = this;
         // Plugin startup logic
         getServer().getPluginManager().registerEvents(new Listeners(), this);
+        httpServer.CC();
         try {
             httpServer.start(null);
         } catch (Exception e) {
@@ -19,5 +22,9 @@ public final class SkyBlockCustomizing extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
         httpServer.stop();
+    }
+
+    public static SkyBlockCustomizing getPlugin() {
+        return plugin;
     }
 }
--- src/main/java/de/developingrene/skyblockcustomizing/httpServer.java
+++ src/main/java/de/developingrene/skyblockcustomizing/httpServer.java
@@ -4,18 +4,34 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
 public class httpServer {
     private static HttpServer server;
+    private static FileConfiguration conf;
+    protected static void CC(){
+        File file = new File(SkyBlockCustomizing.getPlugin().getDataFolder(), "web.yml");
+        conf = YamlConfiguration.loadConfiguration(file);
+        if (!conf.contains("host.port")){
+            conf.set("host.port", Bukkit.getServer().getPort()+1);
+        }
+        try {
+            conf.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) throws Exception{
         start(args);
     }
     public static void start(String[] args) throws Exception {
-        server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server = HttpServer.create(new InetSocketAddress(conf.getInt("host.port")), 0);
         server.createContext("/test" , new testHandler());
         server.setExecutor(null);
         server.start();
@@ -31,7 +47,7 @@ public class httpServer {
             json += "\"Online\": \"true\",\n";
             json += "\"Name\": \""+ Bukkit.getServer().getName() +"\",\n";
             json += "\"Version\": \""+ver[0]+"\",\n";
-            json += "\"OnlinePlayers\": \""+Bukkit.getOnlinePlayers().size()+"\"\n";
+            json += "\"OnlinePlayers\": \""+Bukkit.getOnlinePlayers().size()+"\",\n";
             json += "\"MaxPlayers\": \""+Bukkit.getMaxPlayers()+"\"\n";
             json += "}";
             exchange.sendResponseHeaders(200, json.length());
